96769741111912f250c31ff7594b9ad2e53f9824
msafci <m.elhaggan.fci@gmail.com>
2022-02-22 08:11:26 +0200

    first

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './component/products/products.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'products',pathMatch:'full'},
+  {path:'products', component: ProductsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
